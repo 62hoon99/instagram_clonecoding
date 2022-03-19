7d2da617a4457699f920059d9f60220e14bbe938
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-03-19 18:40:10 +0900

    모달창기능+

--- js/feed_detail_modal.js
+++ js/feed_detail_modal.js
@@ -2,10 +2,13 @@ const bubble_img = document.getElementsByClassName("bubble_img");
 const feed_detail_modal = document.querySelector('.feed_detail_modal');
 const feed_detail_modal_close = document.querySelector('.feed_detail_modal_close');
 const feed_detail_modal_box = document.querySelector('.feed_detail_modal_box');
+const feed_img = document.getElementsByClassName('feed_img');
+const feed_detail_left_img = document.getElementById('feed_detail_left_img');
 
 for (let i = 0; i < bubble_img.length; i++) {
     bubble_img[i].addEventListener('click', () => {
         feed_detail_modal.style.display = "flex";
+        feed_detail_left_img.src = feed_img[i].src;
     })
 }
 
